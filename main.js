fb25891b07fe8447f5d2243ee6ab4d5ce514f2d5
DilawarBaig <dilawaralibaig@gmail.com>
2023-09-25 17:10:04 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+"use strict";
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+const English_marks = {
+    Dilawar: 70,
+    Juned: 99,
+    Saif: 89,
+    Bilal: 98
+};
+const Math_marks = {
+    Dilawar: 79,
+    Juned: 89,
+    Saif: 78,
+    Bilal: 78
+};
+const Urdu_marks = {
+    Dilawar: 67,
+    Juned: 90,
+    Saif: 83,
+    Bilal: 78
+};
+console.log("Marks of students in English subject:");
+console.log(English_marks);
+console.log("Marks of students in Math subject:");
+console.log(Math_marks);
+console.log("Marks of students in Urdu subject:");
+console.log(Urdu_marks);
